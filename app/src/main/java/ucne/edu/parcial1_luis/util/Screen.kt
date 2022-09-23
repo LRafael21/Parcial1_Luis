fce8db2b8cc48b89b8e4235b6355d7cc268f7bb6
Luis <luis_baltodano@ucne.edu.do>
2022-09-22 23:40:14 -0400

    Agregando Object y Rutas a Screen Nav

--- app/src/main/java/ucne/edu/parcial1_luis/util/Screen.kt
+++ app/src/main/java/ucne/edu/parcial1_luis/util/Screen.kt
@@ -2,6 +2,10 @@ package ucne.edu.parcial1_luis.util
 
 sealed class Screen(val route: String){
 
+    sealed class Screen (val route : String){
 
+        object registroparcialscreen : Screen("RegistroParcialScreen")
+        object parciallistscreen : Screen("ParcialListScreen")
+    }
 
 }
