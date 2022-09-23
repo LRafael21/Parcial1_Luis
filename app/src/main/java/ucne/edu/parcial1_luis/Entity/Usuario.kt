076b6a7d8cddb9c9073703540a40937375ae99c1
Luis <luis_baltodano@ucne.edu.do>
2022-09-22 23:01:36 -0400

    Creando carpetas y creando entidad

---
+++ app/src/main/java/ucne/edu/parcial1_luis/Entity/Usuario.kt
@@ -0,0 +1,13 @@
+package ucne.edu.parcial1_luis.Entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Usuario")
+data class Usuario(
+    @PrimaryKey(autoGenerate = true)
+    val UsuarioId: Int = 0,
+    val plantilla1: String,
+    val plantilla2: String,
+    val plantilla3: Double
+)
