d895d144471f1065f863c4972a4441f9db6d42e3
Luis <rafaelbaltodano01@hotmail.com>
2022-09-28 18:53:47 -0300

    Agregando validaciones y TopAppBar a RegistroScreen

--- app/src/main/java/ucne/edu/parcial1_luis/ui/Articulo/Screen/RegistroArticuloScreen.kt
+++ app/src/main/java/ucne/edu/parcial1_luis/ui/Articulo/Screen/RegistroArticuloScreen.kt
@@ -1,14 +1,17 @@
 package ucne.edu.parcial1_luis.ui.Articulo.Screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -18,12 +21,52 @@ fun RegistroArticuloScreen(
 ) {
     var context = LocalContext.current
 
+    fun validar()
+    {
+        if(viewModel.descripcion.isEmpty())
+        {
+            Toast.makeText(
+                context, "Campo descripcion vacio",Toast.LENGTH_LONG
+            ).show()
+        }
+        else if (viewModel.marca.isEmpty())
+        {
+            Toast.makeText(
+                context, "Campo marca vacio",Toast.LENGTH_LONG
+            ).show()
+        }
+        else if (viewModel.marca.isEmpty())
+        {
+            Toast.makeText(
+                context, "Campo Existencia vacio",Toast.LENGTH_LONG
+            ).show()
+        }
+        else if (viewModel.existencia.isDigitsOnly())
+        {
+           viewModel.Guardar()
+            onNavigateBack()
+        }
+        else{
+            Toast.makeText(
+                context, "El campo existencia solo acepta numeros",Toast.LENGTH_LONG
+            ).show()
+
+        }
+
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        TopAppBar(
+            title = { Text(text = "Registro de Articulos") },
+            modifier = Modifier.padding(0.dp),
+            actions = {}
+        )
 
         OutlinedTextField(
             value = viewModel.descripcion,
@@ -52,8 +95,7 @@ fun RegistroArticuloScreen(
                 .fillMaxWidth()
                 .padding(10.dp),
             onClick = {
-                viewModel.Guardar()
-                onNavigateBack()
+               validar()
 
             }) {
             Text(text = "Guardar Articulo")
