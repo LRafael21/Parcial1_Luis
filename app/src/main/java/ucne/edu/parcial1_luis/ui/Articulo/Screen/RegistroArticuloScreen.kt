0d4aae45e1278b5c16d7ca0c3594374028cf1b8f
Luis <rafaelbaltodano01@hotmail.com>
2022-09-28 19:41:45 -0300

    Creando registro de articulos

--- app/src/main/java/ucne/edu/parcial1_luis/ui/Articulo/Screen/RegistroArticuloScreen.kt
+++ app/src/main/java/ucne/edu/parcial1_luis/ui/Articulo/Screen/RegistroArticuloScreen.kt
@@ -2,6 +2,7 @@ package ucne.edu.parcial1_luis.ui.Articulo.Screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,31 +12,51 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun RegistroParcialScreen(
+fun RegistroArticuloScreen(
     onNavigateBack: () -> Unit,
     viewModel: ArticuloViewModel = hiltViewModel()
 ) {
     var context = LocalContext.current
 
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        OutlinedTextField(
+            value = viewModel.descripcion,
+            onValueChange = { viewModel.descripcion = it },
+            label = { Text(text = "Descripcion") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = viewModel.marca,
+            onValueChange = { viewModel.marca = it },
+            label = { Text(text = "Marca") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = viewModel.existencia,
+            onValueChange = { viewModel.existencia = it },
+            label = { Text(text = "Existencia") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
 
         Button(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp)
-            ,
+                .padding(10.dp),
             onClick = {
-                // viewModel.Guadar()
+                viewModel.Guardar()
                 onNavigateBack()
 
             }) {
-            Text(text = "Guardame")
+            Text(text = "Guardar Articulo")
         }
 
     }
